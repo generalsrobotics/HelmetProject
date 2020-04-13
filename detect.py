644524faf9550abe6a91d09d5ce4237f38a6846b
generalsrobotics <60548524+generalsrobotics@users.noreply.github.com>
2020-04-12 22:01:08 -0400

    patch detect class

--- detect.py
+++ detect.py
@@ -15,8 +15,11 @@ class Detector:
         self.depth_scale = None
         self.roi_width = 64
         self.roi_height = 64
-        self.ranges = [20,10,5]
+        self.ranges = [10,7,5]
+        self.rect_color = (0,255,0)     #initialize to green
 
+    def setRanges(self,r1,r2,r3):
+        self.ranges[0] , self.ranges[1], self.ranges[2] = r1, r2, r3 
 
     def startStream(self):
         try:
@@ -25,43 +28,6 @@ class Detector:
             return True
         except:
             return False
-
-    def getROIRect(self,img):
-        y,x,c = img.shape
-        startx = x//2 - self.roi_width//2
-        starty = y//2 - self.roi_height//2
-        roi = img[starty:starty+self.roi_height, startx:startx+self.roi_width, :]
-        out = cv2.rectangle(img, (startx,starty), (startx+self.roi_width,starty+self.roi_height),(0,0,255),1) 
-        return out,np.mean(roi)
-        
-    def getRange(self,dist):
-        r = 0
-        if dist <= self.ranges[2]:
-            r = 2
-        elif dist <= self.ranges[1]:
-            r = 1
-        return r         
-           
-
-    def getROI(self,img,background = None,dist_only = False):
-        y,x,c = img.shape
-        startx = x//2 - self.roi_width//2
-        starty = y//2 - self.roi_height//2
-
-        roi = img[starty:starty+self.roi_height, startx:startx+self.roi_width, :]
-        out = None
-        if dist_only:
-            out = np.mean(roi)
-        else:
-            if background is None:
-                out =  roi
-            else:
-                background[starty:starty+self.roi_height, startx:startx+self.roi_width, :] = roi
-                out = background
-        return out
-
-    def convertDepthToFeet(self,depth_image):
-        return depth_image * self.depth_scale * 3.28084
 
     def getFrame(self):
         try:
@@ -76,6 +42,65 @@ class Detector:
             return color_image, depth_image
         except:
             return None, None   
+
+            
+    def getROIRect(self,img,dist):
+        '''this method now expects a color image to superimpose the rectange on it'''
+        #I modified this method to return the overlay rectangle only and not the mean of the ROI. I am getting the ROI from the getROIdist method instead with appropriate scaling. EC (4/11/20)
+        y,x,c = img.shape
+        startx = x//2 - self.roi_width//2
+        starty = y//2 - self.roi_height//2
+        
+        roi = img[starty : starty + self.roi_height : 1, startx : startx + self.roi_width : 1]
+        
+        if dist <= self.ranges[2]:
+            self.rect_color = (0,0,255)     #set rectangle to red if distance breaks threshold ??
+        else:
+            self.rect_color = (0,255,0)
+            
+        out = cv2.rectangle(img, (startx,starty), (startx+self.roi_width,starty+self.roi_height),self.rect_color,1) 
+        
+        #I am coverting the distance measurement straight into feet in the getROIdist method to avoid multiple computations
+        return out
+
+
+    def getRange(self,dist):
+        ''' r = 0 is no alarm. r = 1 is soft alarm. if r = 2 object is too close'''
+
+        r = 0
+        if dist <= self.ranges[2]:
+            r = 2
+        elif dist <= self.ranges[1]:
+            r = 1
+        return r         
+           
+
+    def getROIdist(self,img,background = None,dist_only = False):
+        '''this method expects a depth image (numpy array)'''
+        y,x = img.shape     #depth image numpy array unpacks two dimensions only 
+        startx = x//2 - self.roi_width//2
+        starty = y//2 - self.roi_height//2
+
+        #roi = img[starty:starty+self.roi_height, startx:startx+self.roi_width, :]  indices had an error
+        roi = img[starty : starty + self.roi_height : 1, startx : startx + self.roi_width : 1]
+        
+        out = None
+        if dist_only:
+            roi_feet = self.depth_scale*roi*3.28084
+            out = np.mean(roi_feet)
+        else:
+            if background is None:
+                out =  roi
+            else:
+                background[starty:starty+self.roi_height, startx:startx+self.roi_width, :] = roi
+                out = background
+        return out
+
+    def convertDepthToFeet(self,depth_image):
+        #we may not need this one here
+        return depth_image * self.depth_scale * 3.28084
+
+
         
     def cleanup():
             self.pipeline.stop()

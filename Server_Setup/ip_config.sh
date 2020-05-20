a01a7c6cb3ca4d73554107b96838c7c5e85a9b26
generalsrobotics <60548524+generalsrobotics@users.noreply.github.com>
2020-05-19 22:57:42 -0400

    Delete ip_config.sh

--- Server_Setup/ip_config.sh
+++
@@ -1,5 +0,0 @@
-sudo ifconfig ra0 192.168.42.1
-sudo mv /etc/dhcp/dhcp.conf /etc/dhcp/dhcp.conf.bak
-sudo cp ./dhcp.conf /etc/dhcp/dhcp.conf
-sudo cp ./isc-dhcp-server /etc/default/isc-dhcp-server
-sudo service isc-dhcp-server restart
